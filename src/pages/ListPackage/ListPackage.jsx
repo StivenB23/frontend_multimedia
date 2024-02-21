d613b93a74e302d3b2a0a6a463a1bd75eef25669
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-02-20 20:03:19 -0500

    Paginador paquetes

--- src/pages/ListPackage/ListPackage.jsx
+++ src/pages/ListPackage/ListPackage.jsx
@@ -3,86 +3,96 @@ import './ListPackage.css';
 import { obtenerPaquetesServicio } from '../../services/paqueteServicio';
 import { obtenerEtiquetasServicio } from '../../services/etiquetasServicio';
 
-const ListPackage = ({ }) => {
-	const [packages, setpackages] = useState([])
-	const [tags, setTags] = useState([])
-	const [packagesFiltered, setpackagesFiltered] = useState([])
-	useEffect(() => {
+const ListPackage = () => {
+    const [packages, setPackages] = useState([]);
+    const [tags, setTags] = useState([]);
+    const [packagesFiltered, setPackagesFiltered] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const packagesPerPage = 10;
+
+    useEffect(() => {
 		const obtenerPaquetes = async () => {
 			const packages = await obtenerPaquetesServicio();
-			const tags = await obtenerEtiquetasServicio();
-			setpackages(packages)
-			setpackagesFiltered(packages)
-			setTags(tags)
+            const tags = await obtenerEtiquetasServicio();
+            setPackages(packages);
+            setPackagesFiltered(packages);
+            setTags(tags);
+        };
+        obtenerPaquetes();
+    }, []);
+	
+	const totalPages = Math.ceil(packagesFiltered.length / packagesPerPage);
+	
+	const handleClick = (type) => {
+		if (type === 'prev' && currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		} else if (type === 'next' && currentPage < totalPages) {
+			setCurrentPage(currentPage + 1);
 		}
-		obtenerPaquetes()
-	}, [])
+	};
 
-	const textLimite = (text, limit = 20) => {
-		const textLimited = text.slice(0, limit);
-		return textLimited
-	}
+    const textLimite = (text, limit = 20) => text.slice(0, limit);
 
-	// const packages = [
-	// 	{ id: 1, name: "Titulo Paquete 1", description: "Descripción del paquete 1", price: "$10", image: "url_de_la_imagen_1" },
-	// 	{ id: 2, name: "Titulo Paquete 2", description: "Descripción del paquete 2", price: "$20", image: "url_de_la_imagen_2" },
-	// 	{ id: 3, name: "Titulo Paquete 3", description: "Para que se muestren dos líneas de texto, necesitas ajustar algunos valores. Primero, asegúrate de que la altura máxima sea el doble de la altura de una línea de texto normal. Luego, para garantizar que solo se muestren dos líneas completas, ajusta el line-height para que coincida con la altura de línea multiplicada por el número deseado de líneas. Aquí está el código actualizado:", price: "$30", image: "url_de_la_imagen_3" },
-	// 	{ id: 3, name: "Titulo Paquete 3", description: "Para que se muestren dos líneas de texto, necesitas ajustar algunos valores. Primero, asegúrate de que la altura máxima sea el doble de la altura de una línea de texto normal. Luego, para garantizar que solo se muestren dos líneas completas, ajusta el line-height para que coincida con la altura de línea multiplicada por el número deseado de líneas. Aquí está el código actualizado:", price: "$30", image: "url_de_la_imagen_3" }
-	// ];
-	const filterPackages = (e) => {
-		let filter = e.target.value;
-		if (filter.length > 0) {
-			const packagesFilter = packages.filter(packageItem => packageItem.nombre.toLowerCase().includes(filter.toLowerCase()))
-			setpackagesFiltered(packagesFilter)
-		} else {
-			setpackagesFiltered(packages)
-		}
-	}
+    const filterPackages = (e) => {
+        const filter = e.target.value;
+        const packagesFilter = packages.filter(packageItem =>
+            packageItem.nombre.toLowerCase().includes(filter.toLowerCase())
+        );
+        setPackagesFiltered(filter.length > 0 ? packagesFilter : packages);
+    };
 
+    const filterTags = (e) => {
+        const filter = e.target.value;
+        const packagesFilter = packages.filter(packageItem =>
+            packageItem.nombre_etiqueta.toLowerCase().includes(filter.toLowerCase())
+        );
+        setPackagesFiltered(filter.length > 0 ? packagesFilter : packages);
+    };
 
-	const filterTags = (e) => {
-		let filter = e.target.value;
-		if (filter.length > 0) {
-			const packagesFilter = packages.filter(packageItem => packageItem.nombre_etiqueta.toLowerCase().includes(filter.toLowerCase()))
-			setpackagesFiltered(packagesFilter)
-		} else {
-			setpackagesFiltered(packages)
-		}
-	}
-	return (
-		<div className='listpackage'>
-			<h2>Paquetes</h2>
-			<div>
-				<input type="text" className='search' onChange={filterPackages} name="" id="" />
-				<div className='filters'>
-					<select name="" onChange={filterTags} id="">
-						<option value="" selected>Etiquetas</option>
-						{tags.map(tag => (
-							<option value={tag.nombre}>{tag.nombre}</option>
-						))}
-					</select>
+    const indexOfLastPackage = currentPage * packagesPerPage;
+    const indexOfFirstPackage = indexOfLastPackage - packagesPerPage;
+    const currentPackages = packagesFiltered.slice(indexOfFirstPackage, indexOfLastPackage);
 
-				</div>
-				<div className="card-container">
-					{/* Mapear los paquetes y renderizar una tarjeta por cada uno */}
-					{packagesFiltered.map(paquete => (
-						<div key={paquete.id} className="card">
-							<div className="card-background">
-							</div>
-							<div className="contentList">
-								<div className="label">{paquete.nombre_etiqueta}</div>
-								<h3>{paquete.nombre}</h3>
-								<p>{textLimite(paquete.descripcion)}...</p>
-
-							</div>
-							<button>Ver más</button>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-
-	);
+    return (
+        <div className='listpackage'>
+            <h2>Paquetes</h2>
+            <div>
+                <input type="text" className='search' onChange={filterPackages} />
+                <div className='filters'>
+                    <select onChange={filterTags} defaultValue="">
+                        <option value="" disabled>Etiquetas</option>
+                        {tags.map(tag => (
+                            <option key={tag.id} value={tag.nombre}>{tag.nombre}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="card-container">
+                    {currentPackages.map(paquete => (
+                        <div key={paquete.id} className="card">
+                            <div className="card-background">
+                          
+                            </div>
+                            <div className="contentList">
+                                <div className="label">{paquete.nombre_etiqueta}</div>
+                                <h3>{paquete.nombre}</h3>
+                                <p>{textLimite(paquete.descripcion)}...</p>
+                            </div>
+                            <button>Ver más</button>
+                        </div>
+                    ))}
+                </div>
+                <div className="pagination">
+                    <button onClick={() => handleClick('prev')} disabled={currentPage === 1}>
+                        Anterior
+                    </button>
+                    <span>Página {currentPage} de {totalPages}</span>
+                    <button onClick={() => handleClick('next')} disabled={currentPage === totalPages}>
+                        Siguiente
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ListPackage;
